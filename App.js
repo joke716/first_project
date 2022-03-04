2a804407419ca94afe7b2cecfbb42556c5ef0cad
Teddy <joke79@hanyang.ac.kr>
2022-03-04 20:44:45 +0900

    test image & textInput

--- App.js
+++ App.js
@@ -21,25 +21,52 @@
 
 
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, Image, TextInput } from 'react-native'
 
 const App = () => {
   return (
-      <View style={styles.container}>
-        <Text style={{ fontWeight: '600', fontSize: 25, color: '#ffffff' }}>
-            Hello world!!!!!
-        </Text>
-      </View>
+      <>
+          <View style={styles.container1}>
+              <Text style={{ fontWeight: '600', fontSize: 25, color: '#ffffff' }}>
+                  Hello world!!!!!
+              </Text>
+              <Image
+                  source={{ uri: "https://reactnative.dev/docs/assets/p_cat2.png" }}
+                  style={{ width: 100, height: 100 }}
+              />
+              <TextInput
+                style={styles.inputStyle}
+                defaultValue={"You can do it!!!"}
+              />
+
+          </View>
+          <View style={styles.container2}>
+            <Text style={{ fontWeight: '600', fontSize: 25, color: '#ffffff' }}>
+                Hello world!!!!!
+            </Text>
+          </View>
+      </>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
+    container1: {
         flex: 1,
         backgroundColor: 'red',
         alignItems: 'center',
         justifyContent: 'center'
     },
+    container2: {
+        flex: 1,
+        backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    inputStyle: {
+        height: 45,
+        borderColor: 'gray',
+        borderWidth: 1.5
+    }
 
 })
 
